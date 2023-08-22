3b60e88ef06978b429e8a17fd1c1b662c205cb37
Daniel <danielmanjarres6@gmail.com>
2023-08-22 15:16:39 -0500

    first commit

---
+++ Guia3.js
@@ -0,0 +1,23 @@
+/*
+alert(" Bienvenido ");
+let cost = Number(prompt("Ingrese el valor del apartamento"));
+let inmueble;
+do {
+    inmueble = Number(prompt("Ingrese el tipo de inmueble 1 usado o 2 nuevo"));
+    
+}while (inmueble != 1 && inmueble!= 2);
+let comision = 0;
+switch (inmueble){
+case 1: 
+ comision = cost * 0.1;
+ break;
+case 2: 
+ comision = cost * 0.2;
+ break;
+}  
+ 
+let propietario = cost - comision;
+let imnobiliaria = comision
+alert("El monto que recibe el propietario es de: $" + propietario + " pesos");
+alert("El monto que recibe el imobiliaria es de: $" + imnobiliaria + " pesos");
+alert(" Hasta la próxima ");*/
